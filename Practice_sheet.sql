142715363f649b4ef3dd8b467ca4f3a381d82c2a
Piyush Wankar <161890058+wpdev2001@users.noreply.github.com>
2025-07-30 10:17:33 +0530

    Update Practice_sheet.sql

--- Practice_sheet.sql
+++ Practice_sheet.sql
@@ -175,3 +175,124 @@ WHERE IsActive=0;
 -- will not work in MySQL because in MySQL, when using a JOIN in a DELETE, 
 -- you must explicitly specify which table you're deleting from — especially
 -- when multiple tables are involved.
+
+-- fetching the details of employees with IDs that appear only once in the table.(with considering duplicate employee ids)
+SELECT *
+FROM Employees
+WHERE EmployeeID IN (
+    SELECT EmployeeID
+    FROM Employees
+    GROUP BY EmployeeID
+    HAVING COUNT(*) = 1
+);
+
+-- creating new employees table to understand LIKE operator
+CREATE TABLE Employees_New (
+    ID INT,
+    Name VARCHAR(50),
+    Department VARCHAR(50)
+);
+
+INSERT INTO Employees_New (ID, Name, Department) VALUES
+(1, 'Alice Johnson', 'Marketing'),
+(2, 'Bob Smith', 'Sales'),
+(3, 'Charlie Brown', 'HR'),
+(4, 'David Lee', 'Finance'),
+(5, 'Eva Thomas', 'Sales'),
+(6, 'Frank Jones', 'HR'),
+(7, 'George Martin', 'Engineering'),
+(8, 'Helen Ford', 'Marketing'),
+(9, 'Ian Black', 'Sales'),
+(10, 'Julia Scott', 'Engineering');
+
+SELECT * FROM Employees_New;
+
+-- Name starts with 'A'
+SELECT * FROM Employees_New WHERE Name LIKE 'A%';
+
+-- Name ends with 'son'
+SELECT * FROM Employees_New WHERE Name LIKE '%son';
+
+-- Name contains 'Lee'
+SELECT * FROM Employees_New WHERE Name LIKE '%Lee%';
+
+-- Name starts with any 5-letter word and ends in 'Scott'
+SELECT * FROM Employees_New WHERE Name LIKE '_____%Scott';
+
+-- Department starts with 'Mar'
+SELECT * FROM Employees_New WHERE Department LIKE 'Mar%';
+
+-- Department ends with 'ing'
+SELECT * FROM Employees_New WHERE Department LIKE '%ing';
+
+-- Department has exactly 5 letters
+SELECT * FROM Employees_New WHERE Department LIKE '_____';
+
+-- Name starts with 'B' and ends with 'h'
+SELECT * FROM Employees_New WHERE Name LIKE 'B%h';
+
+-- Name has ‘o’ as second letter
+SELECT * FROM Employees_New WHERE Name LIKE '_o%';
+
+-- Finding Names that starts from letters A-D 
+-- SELECT * FROM Employees_New WHERE Name LIKE '[A-C]%';
+
+-- NOTE :
+-- The pattern '[A-D]%' only works in SQL Server (T-SQL).
+-- But in Oracle, MySQL, or PostgreSQL, this syntax is not supported by LIKE.
+-- So in order to do that we need to use REGEXP
+SELECT * FROM Employees_New
+WHERE Name REGEXP '^[A-D]';
+
+-- Adding 3 extra columns to learn REGEXP
+ALTER TABLE Employees_New
+ADD COLUMN Age INT AFTER Name,
+ADD COLUMN Email VARCHAR(40),
+ADD COLUMN Salary DECIMAL(10,2);
+
+SELECT * FROM Employees_New;
+
+
+UPDATE Employees_New
+SET Age = 28, Email = 'alice@example.com', Salary = 100000
+WHERE ID = 1;
+
+-- In order to perform REGEXP operations we need the Age,Salary and Email fields data in the existing table
+UPDATE Employees_New
+SET Age = CASE ID 
+			WHEN 1 THEN 28
+            WHEN 2 THEN 30
+            WHEN 3 THEN 26
+            WHEN 4 THEN 29
+            WHEN 5 THEN 31
+            WHEN 6 THEN 32
+            WHEN 7 THEN 27
+            WHEN 8 THEN 33
+            WHEN 9 THEN 25
+            WHEN 10 THEN 30
+            END,
+	Email =  CASE ID
+			WHEN 1 THEN 'alice@example.com'
+            WHEN 2 THEN 'bob@example.com'
+            WHEN 3 THEN 'charlie@example.com'
+            WHEN 4 THEN 'david@example.com'
+            WHEN 5 THEN 'emma@example.com'
+            WHEN 6 THEN 'frank@example.com'
+            WHEN 7 THEN 'grace@example.com'
+            WHEN 8 THEN 'hank@example.com'
+            WHEN 9 THEN 'ivy@example.com'
+            WHEN 10 THEN 'john@example.com'
+            END,
+	Salary = CASE ID
+              WHEN 1 THEN 100000
+              WHEN 2 THEN 95000
+              WHEN 3 THEN 90000
+              WHEN 4 THEN 85000
+              WHEN 5 THEN 88000
+              WHEN 6 THEN 92000
+              WHEN 7 THEN 87000
+              WHEN 8 THEN 91000
+              WHEN 9 THEN 86000
+              WHEN 10 THEN 94000
+           END
+WHERE ID BETWEEN 1 AND 10;
